573f9ba0d5aa7a4dce6a8425ac7669b1ea4b0ee5
Whatahooda <whatahooda@gmail.com>
2023-12-01 12:01:59 -0800

    Changes some colors and the chat tag for the plugin

--- src/main/java/me/whatahooda/itemreturnoncraft/util/CraftReturnUtil.java
+++ src/main/java/me/whatahooda/itemreturnoncraft/util/CraftReturnUtil.java
@@ -5,23 +5,21 @@ import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.format.NamedTextColor;
 
 public class CraftReturnUtil {
+    private static final NamedTextColor TAG_COLOR = NamedTextColor.GOLD;
     private static final NamedTextColor INFO_COLOR = NamedTextColor.WHITE;
     private static final NamedTextColor ERROR_COLOR = NamedTextColor.RED;
 
-    public static final String CRAFT_RETURN_TAG = "[CraftReturn] ";
-    public static final TextComponent COMMAND_NO_PERMISSION = Component.text(CRAFT_RETURN_TAG)
-            .color(ERROR_COLOR)
-            .append(Component.text("You do not have permission to run that command"));
+    public static final TextComponent CRAFT_RETURN_TAG = Component.text("[CraftReturn] ").color(TAG_COLOR);
+    public static final TextComponent COMMAND_NO_PERMISSION = CRAFT_RETURN_TAG
+            .append(Component.text("You do not have permission to run that command").color(ERROR_COLOR));
 
     public static TextComponent messageInfo(String infoMessage) {
-        return Component.text(CRAFT_RETURN_TAG)
-                .color(INFO_COLOR)
-                .append(Component.text(infoMessage));
+        return CRAFT_RETURN_TAG
+                .append(Component.text(infoMessage).color(INFO_COLOR));
     }
 
     public static TextComponent messageError(String errorMessage) {
-        return Component.text(CRAFT_RETURN_TAG)
-                .color(ERROR_COLOR)
-                .append(Component.text(errorMessage));
+        return CRAFT_RETURN_TAG
+                .append(Component.text(errorMessage).color(ERROR_COLOR));
     }
 }
