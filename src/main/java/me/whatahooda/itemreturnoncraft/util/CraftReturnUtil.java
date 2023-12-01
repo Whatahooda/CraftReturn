c4461067d2dadd174bb159ccfb4a2644dfa8737e
Whatahooda <whatahooda@gmail.com>
2023-12-01 11:00:37 -0800

    Changed colors

--- src/main/java/me/whatahooda/itemreturnoncraft/util/CraftReturnUtil.java
+++ src/main/java/me/whatahooda/itemreturnoncraft/util/CraftReturnUtil.java
@@ -2,26 +2,26 @@ package me.whatahooda.itemreturnoncraft.util;
 
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
-import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.format.NamedTextColor;
 
 public class CraftReturnUtil {
-    private static final int INFO_COLOR = 0xFFFFFF;
-    private static final int ERROR_COLOR = 0xAA0000;
+    private static final NamedTextColor INFO_COLOR = NamedTextColor.WHITE;
+    private static final NamedTextColor ERROR_COLOR = NamedTextColor.RED;
 
     public static final String CRAFT_RETURN_TAG = "[CraftReturn] ";
     public static final TextComponent COMMAND_NO_PERMISSION = Component.text(CRAFT_RETURN_TAG)
-            .color(TextColor.color(ERROR_COLOR))
+            .color(ERROR_COLOR)
             .append(Component.text("You do not have permission to run that command"));
 
     public static TextComponent messageInfo(String infoMessage) {
         return Component.text(CRAFT_RETURN_TAG)
-                .color(TextColor.color(INFO_COLOR))
+                .color(INFO_COLOR)
                 .append(Component.text(infoMessage));
     }
 
     public static TextComponent messageError(String errorMessage) {
         return Component.text(CRAFT_RETURN_TAG)
-                .color(TextColor.color(ERROR_COLOR))
+                .color(ERROR_COLOR)
                 .append(Component.text(errorMessage));
     }
 }
